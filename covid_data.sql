be58d0b9a25ee312667f531651aaded6d76bbba0
Batista Junior <junior.engenheirobatista@gmail.com>
2023-04-20 11:35:53 -0300

    Update covid_data.sql

--- covid_data.sql
+++ covid_data.sql
@@ -1,4 +1,4 @@
 select * from covid_data where Recovered is not null;
 
-SELECT COUNT(Recovered) AS total_recovered FROM brasil_data;
+SELECT COUNT(Recovered) AS total_recovered FROM covidl_data;
 select * from covid_data ;
